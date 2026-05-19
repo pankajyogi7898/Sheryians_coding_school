e81d41796e6c31748794bec46ed173e88eaeef1a
PANKAJ NATH <pyogi8461@gmail.com>
2026-05-19 15:34:55 +0530

    newserver

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/" , (req,res)=>{
+    res.send("hello beby")
+})
+app.get("/about",(req,res)=>{
+    res.send("this is about page")
+})
+
+app.listen(3000);
